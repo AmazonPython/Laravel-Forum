5c8069363a6dcd334f069cd9c108f96ee537588f
AmazonPython <amazon3@foxmail.com>
2021-09-06 00:44:12 +0800

    Fixed deletion bug of display profiles page.

--- app/Activity.php
+++ app/Activity.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use function GuzzleHttp\Promise\all;
 
 class Activity extends Model
 {
@@ -13,7 +14,7 @@ class Activity extends Model
         return $this->morphTo();
     }
 
-    public static function feed($user, $take = 50)
+    public static function feed($user, $take = 5)
     {
         return static::where('user_id', $user->id)
             ->latest()
