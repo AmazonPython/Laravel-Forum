ea8219ae967b4ddea983b95c7fecb4aaa7d659d1
AmazonPython <amazon3@foxmail.com>
2021-09-29 18:36:41 +0800

    Limit the maximum number of user replies per minute to 10 and the maximum number of create threads to 3.

--- routes/web.php
+++ routes/web.php
@@ -21,13 +21,13 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('lang/{locale}', 'HomeController@lang')->name('lang');
 
 Route::get('/threads', 'ThreadController@index');
-Route::post('/threads', 'ThreadController@store');
+Route::middleware('throttle:3')->post('/threads', 'ThreadController@store');
 Route::get('/threads/create', 'ThreadController@create');
 Route::get('/threads/{channel}', 'ThreadController@index');
 Route::get('/threads/{channel}/{thread}', 'ThreadController@show');
 Route::delete('/threads/{channel}/{thread}', 'ThreadController@destroy');
 
-Route::middleware('throttle:3')->post('/threads/{channel}/{thread}/replies', 'ReplyController@store');
+Route::middleware('throttle:10')->post('/threads/{channel}/{thread}/replies', 'ReplyController@store');
 Route::patch('/replies/{reply}', 'ReplyController@update');
 Route::delete('/replies/{reply}', 'ReplyController@destroy');
 
