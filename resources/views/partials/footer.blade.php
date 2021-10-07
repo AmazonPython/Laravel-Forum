8956ee71eaceacbf68cc5ac3aa37da1319de7035
AmazonPython <amazon3@foxmail.com>
2021-10-07 21:30:16 +0800

    Updated footer file.

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,9 +1,9 @@
 <footer class="my-5 pt-5 text-muted text-center text-small">
     <p class="mb-1"><font color="#8a2be2">© 2021 - {{ date('Y') }}. Design by Nova with ❤</font></p>
     <ul class="list-inline">
-        <li class="list-inline-item"><a href="#">BeepBeep</a></li>
-        <li class="list-inline-item"><a href="#">Blog</a></li>
-        <li class="list-inline-item"><a href="#">GitHub</a></li>
-        <li class="list-inline-item"><a href="#">Resume</a></li>
+        <li class="list-inline-item"><a href="https://beepbeep.lunanova.top">BeepBeep</a></li>
+        <li class="list-inline-item"><a href="https://lunanova.top">Blog</a></li>
+        <li class="list-inline-item"><a href="https://github.com/AmazonPython">GitHub</a></li>
+        <li class="list-inline-item"><a href="http://lumen.lunanova.top">Resume</a></li>
     </ul>
 </footer>
