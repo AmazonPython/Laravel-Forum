3b94495c73ab9eaceb10e9c45f2b3c2e5389c593
AmazonPython <amazon3@foxmail.com>
2021-10-13 23:41:47 +0800

    Lock thread.

--- app/Thread.php
+++ app/Thread.php
@@ -101,4 +101,9 @@ class Thread extends Model
     {
         $this->update(['best_reply_id' => $reply->id]);
     }
+
+    public function lock()
+    {
+        $this->update(['locked' => true]);
+    }
 }
