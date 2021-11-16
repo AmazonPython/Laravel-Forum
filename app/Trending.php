c677af4d6ead96f8b0cd4fb3ad4d8a707a3f2233
AmazonPython <amazon3@foxmail.com>
2021-11-16 23:27:15 +0800

    Refactor trending to use generic cache driver.

--- app/Trending.php
+++ app/Trending.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\Cache;
 
 class Trending
 {
@@ -13,7 +14,11 @@ class Trending
      */
     public function get()
     {
-        return array_map('json_decode', Redis::zrevrange($this->cacheKey(), 0, 9));
+        //return array_map('json_decode', Redis::zrevrange($this->cacheKey(), 0, 9));
+        return Cache::get($this->cacheKey(), collect())
+            ->sortByDesc('score')
+            ->slice(0, 5)
+            ->values();
     }
 
     /**
@@ -21,7 +26,7 @@ class Trending
      *
      * @return string
      */
-    public function cacheKey()
+    private function cacheKey()
     {
         return 'trending_threads';
     }
@@ -31,13 +36,33 @@ class Trending
      *
      * @param Thread $thread
      */
-    public function push($thread)
+    public function push($thread, $increment = 1)
     {
-        Redis::zincrby($this->cacheKey(), 1, json_encode([
+        /*Redis::zincrby($this->cacheKey(), 1, json_encode([
                 'title' => $thread->title,
                 'path' => $thread->path()
             ])
-        );
+        );*/
+        $trending = Cache::get($this->cacheKey(), collect());
+
+        $trending[$thread->id] = (object) [
+            'score' => $this->score($thread) + $increment,
+            'title' => $thread->title,
+            'path' => $thread->path(),
+        ];
+
+        Cache::forever($this->cacheKey(), $trending);
+    }
+
+    public function score($thread)
+    {
+        $trending = Cache::get($this->cacheKey(), collect());
+
+        if (! isset($trending[$thread->id])) {
+            return 0;
+        }
+
+        return $trending[$thread->id]->score;
     }
 
     /**
@@ -45,6 +70,7 @@ class Trending
      */
     public function reset()
     {
-        Redis::del($this->cacheKey());
+        //Redis::del($this->cacheKey());
+        return Cache::forget($this->cacheKey());
     }
 }
