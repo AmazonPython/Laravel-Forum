96577561fe18af361f05b57d9cf009422f9be3f8
AmazonPython <amazon3@foxmail.com>
2021-11-18 19:09:20 +0800

    Added sidebar blade file, show the trending threads on the thread details page.

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -25,28 +25,9 @@
                     @endforelse
                     {{ $threads->render() }}
                 </div>
-            </div>
+            </div><hr>
         </div>
-        <div class="col-md-4">
-            <div class="card">
-                <div class="card-header">
-                    @lang('messages.threads_trending')
-                </div>
-                <div class="card-body">
-                    <ul class="list-group">
-                        @forelse ($trending as $thread)
-                            <li class="list-group-item">
-                                <a href="{{ url($thread->path) }}" style="color: #333333;">
-                                    {{ $thread->title }}
-                                </a>
-                            </li>
-                        @empty
-                            @lang('messages.threads_no_trending')
-                        @endforelse
-                    </ul>
-                </div>
-            </div>
-        </div>
+        @include('sidebar')
     </div>
 </div>
 @endsection
