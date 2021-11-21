c9d2c791afe927cca8b60ced2c5a59d5f58fcc8e
Nova <amazon3@foxmail.com>
2021-11-21 19:04:05 +0800

    Update Trending.php

--- app/Trending.php
+++ app/Trending.php
@@ -2,22 +2,25 @@
 
 namespace App;
 
-use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\Cache;
 
 class Trending
 {
     /**
-     * 获取所有热门线程.
+     * 获取所有热门线程
      *
      * @return array
      */
     public function get()
     {
-        return array_map('json_decode', Redis::zrevrange($this->cacheKey(), 0, 9));
+        return Cache::get($this->cacheKey(), collect())
+            ->sortByDesc('score')
+            ->slice(0, 5)
+            ->values();
     }
 
     /**
-     * 获取缓存键名.
+     * 获取缓存键名
      *
      * @return string
      */
@@ -27,23 +30,40 @@ class Trending
     }
 
     /**
-     * 将新线程推送到热门线程列表.
+     * 将新线程推送到热门线程列表
      *
      * @param Thread $thread
      */
-    public function push($thread)
+    public function push($thread, $increment = 1)
     {
-        Redis::zincrby($this->cacheKey(), 1, json_encode([
+
+        $trending = Cache::get($this->cacheKey(), collect());
+
+        $trending[$thread->id] = (object) [
+            'score' => $this->score($thread) + $increment,
             'title' => $thread->title,
-            'path' => $thread->path()
-        ]));
+            'path' => $thread->path(),
+        ];
+
+        Cache::forever($this->cacheKey(), $trending);
+    }
+
+    public function score($thread)
+    {
+        $trending = Cache::get($this->cacheKey(), collect());
+
+        if (! isset($trending[$thread->id])) {
+            return 0;
+        }
+
+        return $trending[$thread->id]->score;
     }
 
     /**
-     * 重置所有热门线程.
+     * 重置所有热门线程
      */
     public function reset()
     {
-        Redis::del($this->cacheKey());
+        return Cache::forget($this->cacheKey());
     }
 }
